8aae407d85a02b8aefc002699895c198c0a24fa3
wisdomose <wisdomose05@gmail.com>
2020-12-20 11:50:48 +0100

    cors update

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ require("dotenv").config();
 
 const app=express()
 app.use(express.json())
-app.use(cors());
+app.use("*", cors({
+    origin:["localhost:3000", "localhost:3000/"]
+}));
 
 app.listen(process.env.PORT || 1000,  ()=>console.log("server running"))
 
